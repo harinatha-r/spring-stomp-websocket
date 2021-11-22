c1e7725355f310c92a1ac81adb696c2b838e6fac
Harinatha Radhakrishna <harinatha.radhakrishna@sap.com>
2021-11-22 16:24:51 +0530

    simple websocket example using springboot

---
+++ src/main/java/com/hc/messagingstompwebsocket/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package com.hc.messagingstompwebsocket.controller;
+
+import com.hc.messagingstompwebsocket.dto.Greeting;
+import com.hc.messagingstompwebsocket.dto.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        System.out.println("reached Controller: " + message.getName());
+        Thread.sleep(1000);
+        System.out.println("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
+
---
+++ src/test/java/com/hc/messagingstompwebsocket/MessagingStompWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.messagingstompwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingStompWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
